8f2fa2097852557dad7a9926a01b05573dd806c3
pallobbiswas <pallobbiswas326@gmail.com>
2022-07-18 13:03:26 +0600

    stractur done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styel/styel.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <div className="py-10 md:px-32">
+        <div>
+          <h1 className="text-4xl font-bold ">What We Do</h1>
+          <p className="my-10">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magnam
+            rem velit ducimus ipsum reprehenderit vitae voluptatibus. Sed ullam
+          </p>
+        </div>
+        <div>
+          <div className="first-container p-8 flex justify-center items-center">
+            <div className="second-container">
+              <div className="bg-white w-32 h-20 p-5 container1 rounded-lg">
+                <h1>Lead to lease CRM</h1>
+              </div>
+              <div className="bg-white w-32 h-32 p-5 container2 rounded-lg">
+                <h1>Rasident and tenant portals</h1>
+              </div>
+              <div className="bg-white w-32 h-20 p-5 container3 rounded-lg">
+                <h1>Replace</h1>
+              </div>
+              <div className="bg-white w-32 h-20 p-5 container4 rounded-lg">
+                <h1>Comunity</h1>
+              </div>
+              <div className="bg-white w-32 h-20 p-5 container5 rounded-lg">
+                <h1>Mantainance</h1>
+              </div>
+              <div className="bg-white w-32 h-20 p-5 container6 rounded-lg">
+                <h1>Payment</h1>
+              </div>
+              <div className="bg-white w-32 h-20 p-5 container7 rounded-lg">
+                <h1>Comunication</h1>
+              </div>
+              <div className="bg-white w-60 container8 rounded-lg p-5">
+                <h1>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
+                  et,i
+                </h1>
+                <h3 className=" mt-5 md:mt-20">Payuat</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
